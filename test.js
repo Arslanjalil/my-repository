be61bfbe049f168e4ef9c3ab165a99735ec4fb6a
Your Name <you@example.com>
2023-11-13 18:18:29 +0500

    yesterday work

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log("this is world");
+
+// varibales 
+//  {
+//   var1 =4;
+//   var2 =5;
+//     console.log(var1,var2);
+//  }
+        //{
+        //     let var1 =4;
+        //     console.log(var1);
+        //     {
+        //     let var2 =5;
+        //     console.log(var2);
+        //   }
+        //   console.log(var1,var2);
+        // }
+        // {
+        //     const car=[toyota,Audi,volvo,BMW];
+        // }
+        
