b5afd3ca258edabc646e9f110dd0efd63bb0e685
Your Name <you@example.com>
2023-11-13 19:01:48 +0500

    practice

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
+<html>
+<body>
 
+<h2>this is practice</h2>
+
+<p>JavaScript variables&data types&operators&conditions</p>
 // varibales 
-//  {
+  {
 //   var1 =4;
 //   var2 =5;
 //     console.log(var1,var2);
@@ -18,4 +23,52 @@
         // {
         //     const car=[toyota,Audi,volvo,BMW];
         // }
-        
+                // condition
+                // if (hours>18){
+                //         greeting = "good day"
+                // }
+                // else if (hours>12){
+                //         greeting = "good morning"
+                // }
+                // else if (hours>6){
+                //         greeting = "good afternoon"
+                // }
+                // else{
+                //         greeting = "good night"
+                // }
+                // console.log(greeting);
+                // for loop
+                // for (let i = 0; i < 10; i++) {
+                //     console.log(i);
+                // }
+                           // switch
+                           // {
+                           //     case 1:
+                           //         console.log("one");
+                           //         break;
+                           //     case 2:
+                           //         console.log("two");
+                           //         break;
+                           //     case 3:
+                           //         console.log("three");
+                           //         break;
+                           //     case 4:
+                           //         console.log("four");
+                           //         break;
+                           //     case 5:
+                           //         console.log("five");
+                           //         break;
+                           //     case 6:
+                           //         console.log("six");
+                           //         break;
+                           //     case 7:
+                           //         console.log("seven");
+                           //         break;
+                           //     case 8:
+                           //         console.log("eight");
+                           //         break;
+                           //     case 9:
+                           //         console.log("nine");
+                          //         break;
+</body>
+</html>
